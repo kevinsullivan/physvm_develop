c9f295e9994611a62fd715d7c98455f305d4d0e4
Hannah•Leung <hannah.leung@email.virginia.edu>
2019-01-27 14:39:52 -0500

    temp change. should be updated.

--- src/CodeCoordinate.cpp
+++ src/CodeCoordinate.cpp
@@ -4,15 +4,84 @@
 
 #include "clang/AST/AST.h"
 
-// class VectorASTNode member functions
-void setASTNode(clang::Stmt& vecInstStmt){
-	this->vecInstStmt_ = vecInstStmt;
+// CLASS VECTORASTNODE MEMBER FUNCTIONS IMPLEMENTATION
+
+// VectorASTNode set methods implemetation
+void VectorASTNode::setASTNode(const clang::VarDecl* vecInstStmt, 
+		const MatchFinder::MatchResult &vecInstResult){
+
+	this->ptr_vecInstStmt = vecInstStmt;
+	this->ref_result = vecInstResult;
+}
+
+void VectorASTNode::setASTNodeName(const clang::VarDecl* _vecInstStmt){
+
+	this->name_ = vecInstStmt->getNameAsString();
+
+}
+void VectorASTNode::setASTNodeFilePath(const clang::VarDecl* _vecInstStmt, 
+		const MatchFinder::MatchResult& _ref_result){
+
+	this->filepath_ = _vecInstStmt->getPointOfInstantiation().pringToString(*(_ref_result));
+
+}
+
+void VectorASTNode::setASTNodeMemLoc(const clang::VarDecl* _vecInstStmt, const MatchFinder::MatchResult& _ref_result)
+{
+	return "To be implemented!\n";
+}
+
+// VectorASTNode get methods implemetation
+
+VectorASTNode & VectorASTNode::getASTNode(){
+	return *this;
+}
+
+const string& name VectorASTNode::getName(){
+	return this->name_;
+}
+
+const string& filepath VectorASTNode::getFilePath(){
+	return this->filepath_;
+}
+
+const string& loc VectorASTNode::getDeclLoc(){
+	return this->loc_;
 }
 
 
-clang::Stmt& getASTNode(){
-	return this->vecInstStmt_;
+const string& memLoc_ VectorASTNode::getMemLoc(){
+	return this->memLoc_;
 }
+
+
+
+
+// CLASS EXPRASTNODE MEMBER FUNCTIONS IMPLEMENTATION
+
+
+// CLASS CODECOORDINATES MEMBER FUNCTIONS IMPLEMENTATION
+void CodeCoordinates::InsertVectorASTNodeRef(const VectorASTNode& vecASTNode) {
+	this->VectorASTNodeList.push_back( vecASTNode);
+}
+
+const VectorASTNode& CodeCoordinates::getVectorASTNodeRef(const string& memLocation){
+	for(list<const VectorASTNode& >::iterator it = this->VectorASTNodeList.begin();
+				it != this->VectorASTNodeList.end(); ++it)
+	{
+		if(it->getMemLoc() == memLocation)
+			return it;
+	}
+}
+
+
+
+
+
+
+
+
+
 
 
 
