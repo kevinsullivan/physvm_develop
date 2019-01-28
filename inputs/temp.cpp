8aa143fca642ef5937d671ccce661443c7d79919
Kevin Sullivan <sullivan@virginia.edu>
2019-01-28 11:16:26 -0500

    Added bridge -- not yet working

--- inputs/temp.cpp
+++ inputs/temp.cpp
@@ -36,7 +36,7 @@ using namespace std;
 
 int main(int argc, char **argv){
 
-  Vec v1(1.0,1.0,1.0); // as in frame 1;
+  Vec v1(1.0,1.0,1.0); // as in frame 1; def v1 := mkVec(...)
   Vec v2(2.0,2.0,2.0); // as in frame 2;
 
   Vec v3 = v1.vec_add(v1); // as in the same frame as v1 -- frame 1
