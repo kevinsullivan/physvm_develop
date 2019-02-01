c8f2436da7e3e35910757567ab6f925652562684
Kevin Sullivan <sullivan@virginia.edu>
2019-02-01 17:33:37 -0500

    Code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,6 @@ in main() and updated during the parse tree traversal, as AST
 node handlers are triggered. 
 */
 Oracle         *oracle;
-// Domain         *domain;
 Bridge         *bridge_domain;
 Interpretation *interp;
 
@@ -55,40 +54,10 @@ static llvm::cl::OptionCategory MyToolCategory("Peirce options");
 static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 static cl::extrahelp MoreHelp("No additional options available for Peirce.");
 
-/**********
- * HANDLERS
- *********/
 
-/*
- Vector class
-*/
-class VectorTypeDeclHandler : public MatchFinder::MatchCallback {
-public:
-  virtual void run(const MatchFinder::MatchResult &Result){
-    const CXXRecordDecl *typeVector = 
-      Result.Nodes.getNodeAs<clang::CXXRecordDecl>("VectorTypeDecl");
-    if(typeVector != NULL) {
-      // NO ACTION
-      //cerr << "Found Vec class declaration\n";
-    }
-  }
-};
-
-/*
- Vector.add method
-*/
-class VectorAddMethodDeclHandler: public  MatchFinder::MatchCallback{
-public:
-  virtual void run(const MatchFinder::MatchResult &Result) {
-    // kevin's question is why would this ever be null? -- see clang details
-    const CXXMethodDecl *vecAdd = 
-      Result.Nodes.getNodeAs<clang::CXXMethodDecl>("VectorAddMethodDecl");
-    if(vecAdd != NULL) {
-      // NO ACTION
-      //cerr << "Found Vec::add method declaration\n";
-    }
-  }
-};
+/***************************
+ * CXXConstructExpr HANDLERS
+ ***************************/
 
 /*
 Precondition: consdecl of type CXXConstructExpr* is a pointer to an 
@@ -116,7 +85,9 @@ The cases to be handled include literal and add expressions.
 - Vec v1(0.0,0.0,0.0) is a literal expression
 - (v1.add(v2)).(v3.add(v4)) is an add expression (recursive)
 */
-bridge::Expr* handleExpr(const clang::CXXConstructExpr* consdecl, ASTContext *context, SourceManager& sm) {
+bridge::Expr* handleCXXConstructExpr(
+  const clang::CXXConstructExpr* consdecl, ASTContext *context, SourceManager& sm) {
+
   cout << "Is this a CXXConsDecl dump?\n"; consdecl->dump(); cout << "\n";
   return NULL;
 
@@ -126,7 +97,6 @@ bridge::Expr* handleExpr(const clang::CXXConstructExpr* consdecl, ASTContext *co
   | variable, handle_var
   | add expr, handle_add_expr
  
-
   StatementMatcher matchLit = ...;
   StatementMatcher matchVar = ...;
   Statement Matcher matchAdd = ...;
@@ -141,7 +111,9 @@ bridge::Expr* handleExpr(const clang::CXXConstructExpr* consdecl, ASTContext *co
  */
 } 
 
-bridge::Identifier* handleIdentifier(const VarDecl* vardecl, ASTContext *context, SourceManager& sm) {
+bridge::Identifier* handleCXXConstructIdentifier(
+  const VarDecl* vardecl, ASTContext *context, SourceManager& sm) {
+
   // create a bridge variable object 
   // add interpretation from vardecl to bridge variable object
   // maybe return a bool or something to indicate success or failure?
@@ -150,12 +122,15 @@ bridge::Identifier* handleIdentifier(const VarDecl* vardecl, ASTContext *context
   return bIdent;
 }
 
-void handleIdentifierBinding(bridge::Identifier* bv, bridge::Expr* be) {
+void handleCXXConstructIdentifierBinding(bridge::Identifier* bv, bridge::Expr* be) {
+  // interp->add
 }
 
+/*************************
+ * Handle Vector DeclStmts
+ *************************/
 
-class GeneralVectorHandler: public MatchFinder::MatchCallback{
-
+class VectorDeclStmtHandler: public MatchFinder::MatchCallback {
 public:
   virtual void run(const MatchFinder::MatchResult &Result){
     /*
@@ -178,182 +153,46 @@ public:
       if (vardecl->hasInit()) {
         consdecl = static_cast<const clang::CXXConstructExpr *>(vardecl->getInit());
       }
-      bridge::Expr* be = handleExpr(consdecl, context, sm ); 
-      bridge::Identifier* bi = handleIdentifier(vardecl, context, sm );
-      handleIdentifierBinding(bi, be);
+      bridge::Expr* be = handleCXXConstructExpr(consdecl, context, sm ); 
+      bridge::Identifier* bi = handleCXXConstructIdentifier(vardecl, context, sm );
+      handleCXXConstructIdentifierBinding(bi, be);
     }
     else {
-      cout << "Something's wrong\n";
+      cout << "STUB: Something's wrong\n";
     }
   }
-
-  /*
-
-    // declstmt->dump();
-
-    // get identifier node 
-    const VarDecl* identifier_VarDecl = dyn_cast<VarDecl>(declstmt->getSingleDecl());
-    // get expression node 
-    const clang::Expr *ptr_expression = identifier_VarDecl->getInit();
-
-    // separately the cases of initialization and the assignment
-    const clang::CXXConstructExpr *CCE_expression = static_cast<const clang::CXXConstructExpr *>(ptr_expression);
-
-    // TODO design the matchers to do case analysis instead of hacking
-    // This is not robust code
-    const unsigned numArg = CCE_expression->getNumArgs();
-    
-    if (numArg > 1) 
-      // initialization case
-    {
-      cout<<"initialization!"<<endl;
-      // convert the identifier to Bridge identifier
-
-      // Get file location information
-      ASTContext *context = Result.Context;
-      FullSourceLoc FullLocation = 
-        context->getFullLoc(identifier_VarDecl->getBeginLoc());
-      SourceManager& sm = context->getSourceManager();
-      string where = FullLocation.printToString(sm);
-
-      // HERE'S THE REAL ACTION
-      Space& s = oracle->getSpaceForVector(where); // fix: need filename
-
-      // Create code coordinate object to use in interp
-      VectorASTNode& n = 
-        *new VectorASTNode(declstmt, Result);
-
-      // Create corresponding abstract vector in bridge_domain 
-      const clang::Stmt* vecInstStmt = static_cast<const clang::Stmt*>(declstmt);
-      VecVarExpr& abst_v = bridge_domain->addVecVarExpr(s,vecInstStmt);
-
-      // Connect them through the interpretation
-      interp->putVectorInterp(n, abst_v);
-
-      // Construct Expression
-      // iterate over the children of this node to get the literal values
-      VecLitExpr& vle = bridge_domain-> addVecLitExpr(s);
-
-      for(clang::Stmt::const_child_iterator it = CCE_expression->child_begin();
-                      it!= CCE_expression->child_end(); ++ it)
-      {
-        double value = static_cast<const clang::FloatingLiteral*>(it->IgnoreImplicit())->getValueAsApproximateDouble();
-        vle.addFloatLit(value);
-      }
-      // Add the binding
-      bridge_domain->addLitExprBinding(abst_v,vle);
-    }
-    else
-     // vec_add application
-    {
-      cout<<"vec_add application" <<endl; 
-    }
-    // recursively find the DeclRefExprs
-    // bool VisitDeclRefExpr(DeclRefExpr* DRE) {
-    //   const Decl* D = DRE->getDecl();
-    //   cout<<"Found leave node!\n";
-    //   return true; // returning false will abort the in-depth traversal.
-    // } 
-    // *add expression to the bridge_domain
-    // constructExpression(ptr_expression);
-
-    // *add identifier to bridge_domain
-    
-
-    // *create the binding 
-
-
-  }
-// private:
-  // a class for visiting calls recursively 
-  */
 };
 
-/*******************************************
- * AST Consumer: set up for and handle parse
- *******************************************/
+/********************************************
+ * Top-level analyzer: Match Vector DeclStmts
+ ********************************************/
 
 class MyASTConsumer : public ASTConsumer {
 public:
   MyASTConsumer() {
-
-    /**************
-    Define MATCHERS
-    ***************/
-
-    // Vector class declaration
-    // DeclarationMatcher match_Vector_decl = cxxRecordDecl(hasName("Vec"))
-    //   .bind("VectorTypeDecl");
-
-    // Vector::add method declaration
-    // DeclarationMatcher match_Vector_add_decl = 
-    //   cxxMethodDecl(hasName("vec_add"))
-    //     .bind("VectorAddMethodDecl");
-
-    // OLD HANDLERS THAT HANDLES THE VECTOR CASES SEPARATELY
-    // Vector instance declaration
-    // DeclarationMatcher match_Vector_instance_decl = 
-    //  varDecl(hasInitializer(cxxConstructExpr(hasType(cxxRecordDecl(hasName("Vec")))))).bind("VectorInstanceDecl");
-
-    // Vector::add call
-    // StatementMatcher match_Vector_add_call =
-    //   cxxMemberCallExpr(hasDeclaration(namedDecl(hasName("vec_add"))))
-    //     .bind("VecAddCall");
-
-    // Add the new matcher that matches the Vector cases in general - instance and vec_ad application
     StatementMatcher match_Vector_general_decl = 
       declStmt(hasDescendant(varDecl(hasDescendant(cxxConstructExpr(hasType(asString("class Vec"))))))).bind("VectorStatement");
-
-    /************
-    Bind Handlers
-    ************/
-
-    // Matcher.addMatcher(match_Vector_decl, &HandlerForVecDef);
-    // Matcher.addMatcher(match_Vector_add_decl, &HandlerForVecAddDef);
-    
-    // comment out those handlers that handles vectors cases separately 
-    // Matcher.addMatcher(match_Vector_instance_decl, &HandlerForVecInstanceInit);
-    // Matcher.addMatcher(match_Vector_add_call, &HandlerForVecAdd);
-
-    Matcher.addMatcher(match_Vector_general_decl, &HandlerForVector);
-    
-
+    VectorDeclStmtMatcher.addMatcher(match_Vector_general_decl, &HandlerForVector);
   } 
-
-  /******************************
-  Main Entry Point to ASTConsumer
-  ******************************/
-
-  virtual void Initialize(ASTContext& c) override { context_ = &c; }
   void HandleTranslationUnit(ASTContext &Context) override {
-    Matcher.matchAST(Context);
+    VectorDeclStmtMatcher.matchAST(Context);
   }
-
 private:
-  ASTContext* context_;
-  MatchFinder Matcher;
-  // New Handler class that handles the Vector cases in general,
-  // including the instance and the vec_add application
-  GeneralVectorHandler HandlerForVector;
-
-  // VectorTypeDeclHandler HandlerForVecDef;
-  // VectorAddMethodDeclHandler HandlerForVecAddDef;
-  // VectorInstanceDeclHandler HandlerForVecInstanceInit;
-  // VectorAddCallHandler HandlerForVecAdd;
+  MatchFinder VectorDeclStmtMatcher;
+  VectorDeclStmtHandler HandlerForVector;
 };
 
-/**************************************
-Main entry point into Clang-based tool.
-***************************************/ 
+/*******************************
+* Main Clang Tooling entry point
+********************************/ 
 
 class MyFrontendAction : public ASTFrontendAction {
 public:
   MyFrontendAction() {}
   void EndSourceFileAction() override {
     bool consistent = bridge_domain->isConsistent();
-    cerr << (consistent ? "Good\n" : "Bad\n");
+    cerr << (consistent ? "STUB: Good\n" : "STUB: Bad\n");
   }
-
   std::unique_ptr<ASTConsumer> 
     CreateASTConsumer(CompilerInstance &CI, StringRef file) override {
     return llvm::make_unique<MyASTConsumer>();
@@ -361,19 +200,17 @@ public:
 };
 
 
-/********
- * MAIN *
-********/
+/*****
+* Main
+******/
 
 int main(int argc, const char **argv) {
   CommonOptionsParser op(argc, argv, MyToolCategory);
   ClangTool Tool(op.getCompilations(), op.getSourcePathList());
-
   bridge_domain = new Bridge();
     bridge_domain->addSpace("Space1");
     bridge_domain->addSpace("Space2");
   interp = new Interpretation();
   oracle = new Oracle(*bridge_domain);
-
   return Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
 }
