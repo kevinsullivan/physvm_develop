efb303c42aa099f1176aea5afe91764dda576118
Kevin Sullivan <sullivan@virginia.edu>
2019-02-08 13:56:31 -0500

    Debugged memory bug, stack allocated AST wrapper objects disappearing

--- src/Bridge.cpp
+++ src/Bridge.cpp
@@ -5,41 +5,41 @@
 
 using namespace std;
 using namespace bridge;
- 
-string Space::getName(){return name_;}
 
+string Space::getName() const { return name_; }
 
+const Space &bridge::Expr::getSpace() { return space_; }
 
-const Space& bridge::Expr::getSpace(){return space_;}
-
-
-
-Identifier::Identifier(Space& space, const VarDeclASTNode* vardecl) : 
-    space_(space), vardecl_(vardecl) {
+Identifier::Identifier(Space &space, const VarDeclASTNode *vardecl) : space_(&space), vardecl_(vardecl)
+{
 }
 
+string Identifier::getName()
+{
+    return vardecl_->getVarDecl()->getNameAsString();
+}
 // UNUSED -- fix
 // VecLitExpr class member function implementation
-void VecLitExpr::addFloatLit(float num) {
+void VecLitExpr::addFloatLit(float num)
+{
     cerr << "Stub: add floats to litvalexpr\n";
 }
-
 
 // VecVarExpr class member function implementation
 // const Space& VecVarExpr::getVecVarSpace(){ return space_;}
 
-
 // VecAddExpr class member function implementation
 
 // forward references
-const bridge::Expr& VecAddExpr::getVecAddExprArgL() {
+const bridge::Expr &VecAddExpr::getVecAddExprArgL()
+{
     return arg_left_;
 }
 
-const bridge::Expr& VecAddExpr::getVecAddExprArgR() {
+const bridge::Expr &VecAddExpr::getVecAddExprArgR()
+{
     return arg_right_;
 }
-
 
 // FOR STUBS. Fix/Remove.
 //
@@ -47,15 +47,16 @@ const bridge::Expr& VecAddExpr::getVecAddExprArgR() {
 //	return space_;
 //}
 
-
 // Binding class member function implementation
 //
-const Identifier& Binding::getIdentifier() {
+const Identifier &Binding::getIdentifier()
+{
     return identifier_;
 }
 
-Space& Bridge::addSpace(const string& name) {
-    Space* s = new Space(name);
+Space &Bridge::addSpace(const string &name)
+{
+    Space *s = new Space(name);
     spaces.push_back(*s);
     cerr << "Added space to domain bridge at address " << std::hex << s << "\n";
     return *s;
@@ -78,18 +79,9 @@ bridge::VecLitExpr& Bridge::addLitExpr(Space& s, const LitASTNode* ast) {
 }
 */
 
-// Add new vector, v, in space s, to domain
-// Precondition: s is already in spaces
-// Postcondition: vectors' = vectors + v
-bridge::Expr& Bridge::addVecExpr(Space& s, ExprASTNode* e) {
-    bridge::Expr* be = new bridge::Expr(s, e);
-    expressions.push_back(*be);
-    cerr << "Added Vec Expr to domain bridge key address " << std::hex << e << "\n";
-    return *be;
-}
-
 // precondition: variable already interpreted
-Space& getSpaceOfVarExpr(const ExprASTNode* ast) {
+Space &getSpaceOfVarExpr(const ExprASTNode *ast)
+{
     // look up variable (ast) in identifiers table
     // Space& s = getSpaceOf(ast,identifiers);
     // return s;
@@ -99,53 +91,84 @@ Space& getSpaceOfVarExpr(const ExprASTNode* ast) {
     return *new Space("");
 }
 
-bridge::Expr& Bridge::addVecVarExpr(const ExprASTNode* ast) {
-    Space& s = getSpaceOfVarExpr(ast);
-    bridge::Expr* be = new bridge::Expr(s, ast);
-    expressions.push_back(*be);
-    cerr << "Added Vec Expr to domain bridge key address " << std::hex << &ast << "\n";
-    return *be;
-}
-
-bridge::Expr& Bridge::addVecAddExpr(Space& s, ExprASTNode* e, bridge::Expr& left, bridge:: Expr& right) {
-    bridge::Expr* be = new bridge::VecAddExpr(s, e, left, right);
-    expressions.push_back(*be);
-    cerr << "Added Vec Add Expr to domain bridge key address " << std::hex << e << "\n";
-    return *be;
-}
-
-
-Identifier& Bridge::addIdentifier(Space& s, const VarDeclASTNode* ast)
+bridge::Expr &Bridge::addVecLitExpr(Space &s, ExprASTNode *e)
 {
-    Identifier* id = new Identifier(s, ast);
+    bridge::Expr *be = new bridge::Expr(s, e);
+    cerr << "Adding Vec Lit Expr to domain, address " << std::hex << be << " dump before is ... \n";
+    dump();
+    expressions.push_back(be);
+    cerr << "... dump after is ...\n";
+    dump();
+    return *be;
+}
+
+bridge::Expr &Bridge::addVecVarExpr(const ExprASTNode *ast)
+{
+    Space &s = getSpaceOfVarExpr(ast);
+    bridge::Expr *be = new bridge::Expr(s, ast);
+    cerr << "Adding Vec Var Expr to domain, address " << std::hex << be << " dump before is ... \n";
+    dump();
+    expressions.push_back(be);
+    cerr << "... dump after is ...\n";
+    dump();
+    return *be;
+}
+
+bridge::Expr &Bridge::addVecAddExpr(Space &s, ExprASTNode *e, bridge::Expr &left, bridge::Expr &right)
+{
+    bridge::Expr *be = new bridge::VecAddExpr(s, e, left, right);
+    cerr << "Adding Vec Add Expr to domain, address " << std::hex << be << " dump before is ... \n";
+    dump();
+    expressions.push_back(be);
+    cerr << "... dump after is ... \n";
+    dump();
+    return *be;
+}
+
+Identifier &Bridge::addIdentifier(Space &s, const VarDeclASTNode *ast)
+{
+    Identifier *id = new Identifier(s, ast);
     identifiers.push_back(*id);
-    cerr << "Added Identifier to domain bridge at address " 
-    <<  id << "\n";
+    cerr << "Added Identifier to domain bridge at address "
+         << id << "\n";
     return *id;
 }
 
-
-Binding& Bridge::addBinding(VarDeclASTNode* v, const Identifier& i, 
-                                const bridge::Expr& e)
+Binding &Bridge::addBinding(VarDeclASTNode *v, const Identifier &i,
+                            const bridge::Expr &e)
 {
-    Binding * bd = new Binding(v, i, e);
+    Binding *bd = new Binding(v, i, e);
     bindings.push_back(*bd);
-    cerr << "Added binding from ident at "<< std::hex << &i << " to expr at " << std::hex << &e << " into domain bridge" << endl;
+    cerr << "Added binding from ident at " << std::hex << &i << " to expr at " << std::hex << &e << " into domain bridge" << endl;
     return *bd;
 }
 
+void Bridge::dump()
+{
+    cerr << "BRIDGE DUMP:\n";
+    for (auto e: expressions ){
+        cerr << "Dumping expr at " << std::hex << e << "\n";
+    	if (e != NULL) {
+             cerr << e->toString() << "\n";
+        }
+        else {
+            cerr << "Bridge::dump():: NULL Expr pointer!\n";
+        }
+    }
+}
 // Check domain for consistency
 // Precondition: true
 // Postcondition: return value true indicates presence of inconsistencies
 // Implementation: Call Lean-specific checking code below (make virtual)
-bool Bridge::isConsistent() {
-    Checker* c = new Checker(*this);
+bool Bridge::isConsistent()
+{
+    Checker *c = new Checker(*this);
     bool result = c->Check();
     delete c;
     return result;
 }
 
-vector<Space>& Bridge::getAllSpaces() {
+vector<Space> &Bridge::getAllSpaces()
+{
     return spaces;
 }
-
