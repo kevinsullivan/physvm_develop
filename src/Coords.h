a8dddcc5119f40bc9fbab6f563c0547214c41b93
Kevin Sullivan <sullivan@virginia.edu>
2019-02-17 22:12:37 -0500

    Grappling with mapping from coords back to AST objects. It has to go through at least two disjoint relations, one for objects that inhereit from Clang Decl and one for  objects that inherit from Clang Stmt objects.

--- src/Coords.h
+++ src/Coords.h
@@ -1,26 +1,41 @@
 #ifndef COORDS_H
 #define COORDS_H
 
+#include "AST.h"
 #include "clang/AST/AST.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include <cstddef>
 #include <iostream> // for cheap logging only
-#include "AST.h"
-
 
 /*
-Code coordinates provide for ontology translation, between the 
-concrete types used to represent pertinent code elements in a 
-given programming language and system (code language), and the 
-abstract terms of a domain language. Here the code language is
-Clang as used to map applications built on our simple vector
-class (Vec). The domain language is one of simple vector space
-expressions and objects. 
+Code coordinates provide for ontology translation, between the AST types used to represent pertinent 
+code elements, and the AST types used to represent
+pertinent domain elements. 
+
+Here the AST language a domain-driven projection of
+Clang. The domain language is one of vector space
+expressions, values, identifiers, and definitions.
+
+Code coordinates are mapped to domain elements via
+a relation represented externally to Coords objects,
+namely in the coords2domain relation(s). AST values
+are mapped to Coords by ast2coordinates externalized
+relations. The mapping back from coordinates to the
+underlying AST objects is implemented by references
+contained within these coords objects. Each object
+represents with dynamic state the kind of AST-level
+object it maps to, and provides a pointer back to 
+that object.
+
+The challenge is to get precisely typed references
+to AST elements back from Coords objects. But in the
+current application, we don't really need to do so.
+In future, dynamic casts seem likely to be useful.
 */
 
-//using namespace clang;
-//using namespace clang::ast_matchers;
-//using namespace std;
+// using namespace clang;
+// using namespace clang::ast_matchers;
+// using namespace std;
 
 namespace coords {
 
@@ -31,38 +46,35 @@ enum ast_type { CLANG_AST_STMT, CLANG_AST_DECL };
 
 class Coords {
 public:
-
-  Coords(const clang::Stmt *stmt); 
+  Coords(const clang::Stmt *stmt);
   Coords(const clang::Decl *decl);
 
-  // Note: Clang architecture showing through here. Clang AST base types. 
   const clang::Stmt *getClangStmt() const;
   const clang::Decl *getClangDecl() const;
 
   virtual bool operator==(const Coords &other) const;
   virtual std::string toString() const;
 
-  private:
-    // TODO: Make it a tagged union to save some space
-    ast_type tag_;
-    clang::Stmt *clang_ast_stmt_;
-    clang::Decl *clang_ast_decl_;
+  bool astIsClangDecl() { return (ast_type_ == CLANG_AST_DECL); }
+
+  // Remember: Clang Expr inherits from Stmt
+  bool astIsClangStmt() { return (tag_ == CLANG_AST_STMT); }
 
 private:
-  ast_type ast_type_;
+  ast_type ast_type_tag_;
   const clang::Stmt *clang_stmt_;
   const clang::Decl *clang_decl_;
 };
 
-struct CoordsHasher; 
+struct CoordsHasher;
 
 /*************************************************************
-* Coordinate subclasses, for type checking, override behaviors
-*************************************************************/
+ * Coordinate subclasses, for type checking, override behaviors
+ *************************************************************/
 
 /******
-* Ident
-******/
+ * Ident
+ ******/
 
 class VecIdent : public Coords {
 public:
@@ -72,9 +84,8 @@ public:
 };
 
 /*****
-* Expr
-*****/
-
+ * Expr
+ *****/
 
 // TODO: Add a dynamic type tag here
 class VecExpr : public Coords {
@@ -84,33 +95,34 @@ public:
   virtual std::string toString() const;
 };
 
-/* 
+/*
 No such intermediate node in Clang AST.
 Goes straight to CXXConstructExpr. Use
 Vector_Lit.
 */
 class VecLitExpr : public Coords {};
 
-
 class VecVarExpr : public VecExpr {
 public:
   VecVarExpr(const clang::DeclRefExpr *d);
   clang::DeclRefExpr *getDeclRefExpr();
   virtual std::string toString() const;
+
 private:
   coords::Coords *var_; // TODO: Fix
 };
 
-  // TODO: add accessors for left and right?
+// TODO: add accessors for left and right?
 class VecVecAddExpr : public VecExpr {
 public:
-  VecVecAddExpr(const clang::CXXMemberCallExpr *mce, coords::Coords *mem, coords::Coords *arg);
+  VecVecAddExpr(const clang::CXXMemberCallExpr *mce, coords::Coords *mem,
+                coords::Coords *arg);
   clang::CXXMemberCallExpr *CXXMemberCallExpr();
   virtual std::string toString() const;
+
 private:
   coords::Coords *mem_;
   coords::Coords *arg_;
-
 };
 
 enum VectorCtorType { VEC_CTOR_LIT, VEC_CTOR_EXPR, VEC_CTOR_VAR };
@@ -121,15 +133,17 @@ public:
   const clang::CXXConstructExpr *getCXXConstructExpr() const;
   VectorCtorType getVectorType();
   virtual std::string toString() const;
+
 protected:
-    const VectorCtorType tag_;
+  const VectorCtorType tag_;
 };
 
 // TODO: methods to get x, y, z
 class Vector_Lit : public Vector {
 public:
-  Vector_Lit(clang::CXXConstructExpr* ast, float x, float y, float z);
+  Vector_Lit(clang::CXXConstructExpr *ast, float x, float y, float z);
   virtual std::string toString() const;
+
 private:
   float x_;
   float y_;
@@ -138,41 +152,43 @@ private:
 
 class Vector_Var : public Vector {
 public:
-  Vector_Var(clang::CXXConstructExpr* ast, const coords::VecVarExpr* expr);
+  Vector_Var(clang::CXXConstructExpr *ast, const coords::VecVarExpr *expr);
   virtual std::string toString() const;
-  VecVarExpr*  getVecVarExpr();
+  VecVarExpr *getVecVarExpr();
+
 private:
-  VecVarExpr* expr_;
+  VecVarExpr *expr_;
 };
 
 // change name to VecVecAddExpr? Or generalize from that a bit.
 class Vector_Expr : public Vector {
 public:
-  Vector_Expr(const clang::CXXConstructExpr ast, coords::Vector_Expr* expr);
+  Vector_Expr(const clang::CXXConstructExpr ast, coords::Vector_Expr *expr);
   virtual std::string toString() const;
-  Vector_Expr* getVector_Expr();
+  Vector_Expr *getVector_Expr();
+
 private:
-  Vector_Expr* expr_;
+  Vector_Expr *expr_;
 };
 
-
 /****
-* Def
-****/
+ * Def
+ ****/
 
 class Vector_Def : public Coords {
 public:
-  Vector_Def(const clang::DeclStmt def, coords::VecIdent *bv, coords::VecExpr *be);
+  Vector_Def(const clang::DeclStmt def, coords::VecIdent *bv,
+             coords::VecExpr *be);
   const clang::DeclStmt *getDeclStmt() const;
   coords::VecIdent *getIdent() const;
   coords::VecExpr *getExpr() const;
   virtual std::string toString() const;
-  private:
+
+private:
   VecIdent *bv_;
   VecExpr *be_;
 };
 
-} // namespace codecoords
-
+} // namespace coords
 
 #endif
