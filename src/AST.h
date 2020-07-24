36b9be42cb200867215e1db1f2c306d1814ae244
Kevin Sullivan <sullivan.kevinj@gmail.com>
2020-07-24 18:25:23 +0000

    Added comments to AST.h to documented the current and/or intended uses/meanings of the different constructs being matched in C++ code.

--- src/AST.h
+++ src/AST.h
@@ -33,16 +33,41 @@ Physically relevant expression, e.g., a Real3 expression
 using EXPR = const clang::Stmt;
 
 /*
-
+Intended to match on tf::Point tf::Vector3.
+Implemented but doesn't handle a bunch of stuff.
+Actual ROS applications will have many different
+constructs that should map here. Currently handles
+just instances of these two classes, but in a
+practical system would also have to handle
+ROS messages [geometry_msgs::*], stamped versions,
+uses of other libraries, such as Eigen, etc.
 */
 using REAL3_EXPR = const clang::Stmt;
 using REAL1_EXPR = const clang::Stmt;
+
+/*
+Not currently matching.
+*/
 using REALMATRIX_EXPR = const clang::Stmt;
+
+/*
+Intended for matching quaternions and
+homogeneous coordinate tuples.
+*/
 using REAL4_EXPR = const clang::Stmt;
 
-// ANDREW: move under imperative?
+/*
+Currently not used for matching. Matching will
+be done with more special case matchers below.
+*/
 using ASSIGNMENT = const clang::Stmt;
 
+/*
+Not currently used or clearly intended to be used.
+These are special cases of assignment. Currently no
+assignment matchers. Currently do have matching of
+declarations with initializations.
+*/
 using ASSIGN_REAL1_VAR_REAL1_EXPR = const clang::Stmt;
 using ASSIGN_REAL3_VAR_REAL3_EXPR = const clang::Stmt;
 using ASSIGN_REAL4_VAR_REAL4_EXPR = const clang::Stmt;
@@ -59,6 +84,9 @@ using DECL_REAL4_VAR = const clang::Stmt;
 
 using DECL_REALMATRIX_VAR = const clang::Stmt;
 
+/*
+Intended to match all instances of tfScalar.
+*/
 using REAL1_EXPR = const clang::Stmt;
 using PAREN_REAL1_EXPR = const clang::Stmt;
 using INV_REAL1_EXPR = const clang::Stmt;
@@ -68,6 +96,7 @@ using SUB_REAL1_EXPR_REAL1_EXPR = const clang::Stmt;
 using MUL_REAL1_EXPR_REAL1_EXPR = const clang::Stmt;
 using DIV_REAL1_EXPR_REAL1_EXPR = const clang::Stmt;
 using REF_REAL1_VAR = const clang::Stmt;
+
 
 using REAL3_EXPR = const clang::Stmt;
 using PAREN_REAL3_EXPR = const clang::Stmt;
@@ -94,7 +123,6 @@ using REF_EXPR_REALMATRIX_VAR = const clang::Stmt;
 using REAL1_VAR_IDENT = const clang::VarDecl;
 using REAL3_VAR_IDENT = const clang::VarDecl;
 using REAL4_VAR_IDENT = const clang::VarDecl;
-
 using REALMATRIX_VAR_IDENT = const clang::VarDecl;
 
 using REAL1_LITERAL = const clang::Stmt;
