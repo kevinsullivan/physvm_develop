efa345fa6b842345179b7241691bf74834c43c03
Kevin Sullivan <sullivan.kevinj@gmail.com>
2021-01-14 18:08:31 +0000

    Add documentation to transform, improve system docs.

--- ros_test_cases/README.md
+++ ros_test_cases/README.md
@@ -1,3 +1,3 @@
 # ROS Test Cases for Peirce
 
-This project contains a set of ROS projects, each of which can be compiled and run separately, and that constitute test cases for Peirce. See the documentation called HOW_TO_CREATE_COMPILE_RUN_NEW_ROS_TEST_PROJECT.md for information on how to create, compile, run, and update these projects.
+This project contains a set of ROS projects, each of which can be compiled and run separately, and that constitute test cases for Peirce. See the documentation called HOW_TO_CREATE_COMPILE_RUN_NEW_ROS_TEST_PROJECT.md for information on how to create, compile, run, update, and analyze (using Peirce) these projects.
--- ros_test_cases/playground/src/playground/src/transforms.cpp
+++ ros_test_cases/playground/src/playground/src/transforms.cpp
@@ -1,4 +1,14 @@
 
+/*
+This program provides examples involving the creation
+and application of Tf transforms. For documentation see
+http://docs.ros.org/en/kinetic/api/tf/html/c++/classtf_1_1Transform.html#details.
+Briefly, "The Transform class supports rigid transforms
+with only translation and rotation and no scaling/shear. 
+It can be used in combination with Vector3, Quaternion and
+Matrix3x3 linear algebra classes." 
+*/
+
 #include "ros/ros.h"
 #include "geometry_msgs/Vector3Stamped.h"
 #include "geometry_msgs/Vector3.h"
@@ -9,15 +19,75 @@
 //#include "nav_msgs/MapMetaData.h"
 //#include "nav_msgs/OccupancyGrid.h"
 //#include "nav_msgs/GetMap.h"
-
-
 #include <cmath>
 
 
 int main(int argc, char **argv){
-    ros::init(argc, argv, "velocity");
-    ros::NodeHandle node;  
-    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug);
+    // Initialize ROS and provide name for this Node
+    ros::init(argc, argv, "transformation_tests");
+
+    // Create ROS node (calling ros::start() when first node is created)
+    ros::NodeHandle node;
+
+    // Set ROS logging level to DEBUG 
+    ros::console::set_logger_level(
+                        ROSCONSOLE_DEFAULT_NAME, 
+                        ros::console::levels::Debug);
+    ros::console::notifyLoggerLevelsChanged();
+
+    /*
+    Unit and coordinate conventions used in ROS:
+    https://www.ros.org/reps/rep-0103.html. 
+    
+    It notes that "Inconsistency in units and 
+    conventions is a common source of integration 
+    issues for developers and can also lead to 
+    software bugs."
+
+    Ros standardizes on the SI unit system. Base
+    units are meter, kilogram, second, ampere.
+    Derived units are radian, hertz, newton, watt,
+    volt, celsius, and tesla.
+
+    Coordinate frame conventions vary depending
+    on intended use, including (forward, left, up),
+    (east, north, up), (right, down, forward) for
+    camera suffix frames.
+
+    Representations of rotations are several:
+    quaternion, rotation matrix, roll/pitch/ya,
+    and Euler angles (and for geograpic coords
+    east is at yaw=0 increasing counter-clockise,
+    which differs from the usual compass bearing 
+    has north at yaw=0 and increases clockwise).
+
+    There's more, on covariance matrix reps. See
+    the documentation.
+    */
+
+    /*
+    Frame conventions are documented here:
+    https://www.ros.org/reps/rep-0105.html.
+    "Developers of drivers, models, and libraries
+    need a share convention for coordinate frames
+    in order to better integrate and re-use 
+    software components." An understatement!
+
+    ROS uses a number of standardized frames.
+    - base_link (per vehicle)
+    - odom (per vehicle)
+    - map
+    - earth
+
+    More. TBD.
+    */
+    
+    /*
+    KS: Problem: At this point in the execution of
+    this program we have implicit content in the 
+    form of a "world" geometric space and a world  implicit
+    time space
+    */
 
     //world geom
     //bind std
