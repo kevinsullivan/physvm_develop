b8d9f1fa2d25778ee7fd63cd2c6ba28d3fefdd67
Kevin Sullivan <sullivan.kevinj@gail.com>
2021-03-21 15:04:46 +0000

    .

--- new/spaces/space_framed.lean
+++ new/spaces/space_framed.lean
@@ -49,6 +49,53 @@ Constants
 -/
 def vectr_zero {f : fm K} (s : spc K f ) := mk_vectr K s 0
 
+variables {f : fm K} { s : spc K f}
+
+/-
+Replaced the following code. We really don't want code that looks like this.
+instance : has_add (vectr K s) := ⟨λv1 v2, ⟨⟨v1.1.1,v1.1.2,v1.coord + v2.coord⟩⟩⟩
+instance : has_zero (vectr K s) := ⟨⟨vec_zero K⟩⟩
+instance : has_neg (vectr K s) := ⟨λv, ⟨⟨v.1.1,v.1.2,-v.1.3⟩⟩⟩
+-/
+instance : has_add (vectr K s) := ⟨add_vectr_vectr K⟩
+instance : has_zero (vectr K s) := ⟨vectr_zero K s⟩
+instance : has_neg (vectr K s) := ⟨neg_vectr K⟩
+instance aff_comm_group_framed : add_comm_group (vectr K s) := sorry
+@[ext]
+def vec_scalar_framed : K → vectr K s → vectr K s :=
+    mul_vectr K 
+instance : has_scalar K (vectr K s) := ⟨mul_vectr K ⟩
+instance : mul_action K (vectr K s) := ⟨sorry, sorry⟩
+instance : distrib_mul_action K (vectr K s) := ⟨sorry,sorry⟩
+instance aff_semimod_framed : semimodule K (vectr K s) := ⟨sorry, sorry⟩
+instance aff_module_framed : module K (vectr K s) := aff_semimod_framed K
+def aff_group_action_framed : vectr K s → point K s → point K s :=
+    add_vectr_point K
+def aff_group_sub_framed : point K s → point K s → vectr K s :=
+    sub_point_point K
+instance : has_vadd (vectr K s) (point K s) := ⟨aff_group_action_framed K⟩
+instance : has_vsub (vectr K s) (point K s) := ⟨aff_group_sub_framed K⟩
+instance : add_action (vectr K s) (point K s) := ⟨
+    aff_group_action_framed K, 
+    sorry, 
+    sorry⟩
+instance : nonempty (point K s) := ⟨⟨pt_zero K⟩⟩
+
+
+instance aff_torsor_framed : add_torsor (vectr K s) (point K s) := 
+⟨sorry,
+sorry,
+sorry,
+sorry,
+sorry,
+sorry⟩
+
+
+instance aff_coord_is_framed : 
+    affine_space
+        (vectr K s) 
+        (point K s) := 
+    aff_torsor_framed K
 /-
 
 /-
@@ -96,69 +143,5 @@ instance my_affine_space : affine_space (vec K) (pt K) :=
 aff_torsor K 
 -/
 
-variables {f : fm K} { s : spc K f}
 
-/-
-Replaced the following code. We really don't want code that looks like this.
-instance : has_add (vectr K s) := ⟨λv1 v2, ⟨⟨v1.1.1,v1.1.2,v1.coord + v2.coord⟩⟩⟩
-instance : has_zero (vectr K s) := ⟨⟨vec_zero K⟩⟩
-instance : has_neg (vectr K s) := ⟨λv, ⟨⟨v.1.1,v.1.2,-v.1.3⟩⟩⟩
--/
-instance : has_add (vectr K s) := ⟨add_vectr_vectr K⟩
-instance : has_zero (vectr K s) := ⟨vectr_zero K s⟩
-instance : has_neg (vectr K s) := ⟨neg_vectr K⟩
-instance aff_comm_group_framed : add_comm_group (vectr K s) :=
-sorry
-@[ext]
-def vec_scalar_framed : K → vectr K s → vectr K s :=
-    mul_vectr K 
-
-instance : has_scalar K (vectr K s) := ⟨vec_scalar_framed K⟩
-
-instance : mul_action K (vectr K s) := ⟨sorry, sorry⟩
-
-instance : distrib_mul_action K (vectr K s) := ⟨sorry,sorry⟩
-
-instance aff_semimod_framed : semimodule K (vectr K s) := ⟨sorry, sorry⟩
-
-instance aff_module_framed : module K (vectr K s) := aff_semimod_framed K
-
-/-! ### group action of aff_vec_coord_tuple on aff_pt_coord_tuple -/
-
-
-def aff_group_action_framed : vectr K s → point K s → point K s :=
-    add_vectr_point K
-
-
-def aff_group_sub_framed : point K s → point K s → vectr K s :=
-    sub_point_point K
-
-#check add_action
-
-instance : has_vadd (vectr K s) (point K s) := ⟨aff_group_action_framed K⟩
-
-instance : has_vsub (vectr K s) (point K s) := ⟨aff_group_sub_framed K⟩
-
-instance : add_action (vectr K s) (point K s) := ⟨
-    aff_group_action_framed K, 
-    sorry, 
-    sorry⟩
-
-
-instance : nonempty (point K s) := ⟨⟨pt_zero K⟩⟩
-
-instance aff_torsor_framed : add_torsor (vectr K s) (point K s) := 
-⟨sorry,
-sorry,
-sorry,
-sorry,
-sorry,
-sorry⟩
-
-
-instance aff_coord_is_framed : 
-    affine_space
-        (vectr K s) 
-        (point K s) := 
-    aff_torsor_framed K
 
