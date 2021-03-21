a996cf2a5e10459d85b7b62c1006275714fcadfb
Kevin Sullivan <sullivan.kevinj@gail.com>
2021-03-21 02:42:47 +0000

    Contribute some corrections to Andrew code a few hours after he stopped working on Saturday

--- new/spaces/space_unframed.lean
+++ new/spaces/space_unframed.lean
@@ -6,50 +6,77 @@ variables
 (K : Type u) [ring K] [inhabited K]
 
 /-
-Frameless points, vectors
--/
+I replaced these structures with
+structures that extend from K ⨯ K
+by adding the invariants field.
+
 structure vec :=
 (isPt  : K)
 (inv : isPt = (0:K))
 (coord : K)
-
-#check vec
 
 structure pt :=
 (isPt  : K)
 (inv : isPt = (1:K))
 (coord : K)
 
-def mk_vec (k : K) : vec K := vec.mk 0 rfl k
-def mk_pt (k : K) : pt K  := pt.mk 1 rfl k
+The result of this little rewrite
+led to a simple linear 2 space library. 
+Here it is. We'll take it as a basis
+for representing Affine 1 space. And 
+from there we'll use direct sums.
+-/
+
 
 /-
-Affine space operations
+Linear 2 space
 -/
-def mul_vec (k : K) (v : vec K) [has_mul K] : vec K := mk_vec K (k * v.coord)
-def neg_vec (k : K) (v : vec K) [has_mul K] : vec K := mk_vec K (-1 * v.coord)
-def add_vec_vec (k : K) (v1 v2 : vec K) [has_add K] : vec K := mk_vec K (v1.coord + v2.coord)
-def add_pt_vec (k : K) (p : pt K) (v : vec K) [has_add K] : pt K := mk_pt K (p.coord + v.coord)
-def add_vec_pt (v : vec K) (p : pt K) : pt K := mk_pt K (p.coord + v.coord)
-def sub_pt_pt (p1 p2 : pt K) : vec K := mk_vec K (p2.coord - p1.coord)
+
+def lin2 := prod K K
+
+def lin2_add : lin2 K → lin2 K → lin2 K
+| (f1,s1) (f2,s2) := ⟨ f1 + f2, s1 + s2 ⟩ 
+
+def lin2_mul : K → lin2 K → lin2 K
+| a (f,s) := ⟨ a * f, a * s ⟩ 
+
+instance : has_add (lin2 K) := ⟨ lin2_add K ⟩ 
+instance : has_zero (lin2 K) := ⟨ (0,0) ⟩ 
+instance : has_scalar K (lin2 K) := ⟨ lin2_mul K ⟩ 
+
+
+--      Affine 1 space
+
+-- point, vector
+structure pt extends K × K := (inv : fst = 1)
+def mk_pt (k : K) : pt K  := pt.mk 1 rfl
+structure vec extends K × K := (inv : fst = 0)
+def mk_vec (k : K) : vec K := vec.mk 0 rfl
+
+-- operations
+def mul_vec [has_mul K] (k : K) (v : vec K) : vec K := mk_vec K (k * v.to_prod.2)
+def neg_vec  [has_mul K] (v : vec K) : vec K := mk_vec K (-1 * v.to_prod.2)
+def add_vec_vec [has_add K] (v1 v2 : vec K) : vec K := mk_vec K (v1.to_prod.2 + v2.to_prod.2)
+def add_pt_vec [has_add K] (p : pt K) (v : vec K) : pt K := mk_pt K (p.to_prod.2 + v.to_prod.2)
+def add_vec_pt (v : vec K) (p : pt K) : pt K := mk_pt K (p.to_prod.2 + v.to_prod.2)
+def sub_pt_pt (p1 p2 : pt K) : vec K := mk_vec K (p2.to_prod.2 - p1.to_prod.2)
 
 -- TODO: add affine combination operation here 
 
-/-
-One values for pts and vecs 
--/
+--Standard values 
 def pt_zero   := mk_pt K 0
 def vec_zero  := mk_vec K 0
 def vec_one   := mk_vec K 1
 def std_pt : pt K    := pt_zero K
 def std_vec : vec K  := vec_one K
 
-/-
-Instantiate affine_space typeclass 
--/
+
+-- Implement the affine_space typeclass 
 open_locale affine
 section old_structure_cmd
 set_option old_structure_cmd true
+
+
 /--/
 instance : inhabited (pt K) := ⟨ pt_zero K ⟩ 
 instance : has_zero (vec K) := ⟨ vec_zero K ⟩ 
@@ -82,11 +109,13 @@ aff_vadd_vsub K
 ⟩
 
 instance my_affine_space : affine_space (vec K) (pt K) :=  
-aff_torsor K -/
+aff_torsor K 
+-/
 
-instance : has_add (vec K) := ⟨λv1 v2, ⟨v1.1,v1.2,v1.coord + v2.coord⟩⟩
+
+instance : has_add (vec K) := ⟨ add_vec_vec K ⟩  --⟨λv1 v2, ⟨v1.1, v1.2, v1.to_prod.2 + v2.to_prod.2⟩⟩
 instance : has_zero (vec K) := ⟨vec_zero K⟩
-instance : has_neg (vec K) := ⟨λv, ⟨v.1,v.2,-v.3⟩⟩
+instance : has_neg (vec K) := ⟨neg_vec K⟩
 
 /-! ### Type class instance for abelian group -/
 instance aff_comm_group : add_comm_group (vec K) :=
@@ -97,8 +126,8 @@ sorry
 
 
 @[ext]
-def vec_scalar : K → vec K → vec K :=
-    λ a x, ⟨x.1,x.2,a*x.3⟩
+def vec_scalar : K → vec K → vec K := mul_vec K
+--    λ a x, ⟨x.1,x.2,a*x.3⟩
 
 instance : has_scalar K (vec K) := ⟨vec_scalar K⟩
 
@@ -113,12 +142,12 @@ instance aff_module : module K (vec K) := aff_semimod K
 /-! ### group action of aff_vec_coord_tuple on aff_pt_coord_tuple -/
 
 
-def aff_group_action : vec K → pt K → pt K :=
-    λ x y, ⟨y.1,y.2,x.3+y.3⟩
+def aff_group_action : vec K → pt K → pt K := add_vec_pt K
+--    λ x y, ⟨y.1,y.2,x.3+y.3⟩
 
 
-def aff_group_sub : pt K → pt K → vec K :=
-    λ x y, ⟨x.1-y.1,sorry,x.3-y.3⟩
+def aff_group_sub : pt K → pt K → vec K := sub_pt_pt K
+--    λ x y, ⟨x.1-y.1,sorry,x.3-y.3⟩
 
 #check add_action
 
