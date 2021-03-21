4cf6b366c89b926148f79fd96309b57789fc1ffd
Kevin Sullivan <sullivan.kevinj@gail.com>
2021-03-21 14:56:04 +0000

    Restored and fixed affine space API implementation

--- new/spaces/space_framed.lean
+++ new/spaces/space_framed.lean
@@ -37,16 +37,17 @@ fm.deriv (p.to_pt, v.to_vec) parent   -- TODO: make sure v ≠ 0
 Operations
 -/
 
-
-/-
-Affine space operations
--/
 def mul_vectr {f : fm K} {s : spc K f } (k : K) (v : vectr K s) : vectr K s := mk_vectr K s (k * v.to_vec.to_prod.2) -- yuck
-def neg_vectr {f : fm K} {s : spc K f } (k : K) (v : vectr K s) : vec K := mk_vec K (-1 * v.to_vec.to_prod.2)
-def add_vectr_vectr {f : fm K} {s : spc K f } (k : K) (v1 v2 : vectr K s) : vec K := mk_vec K (v1.to_vec.to_prod.2 + v2.to_vec.to_prod.2)
-def add_point_vectr {f : fm K} {s : spc K f } (k : K) (p : point K s) (v : vectr K s) : point K s := mk_point K s (p.to_pt.to_prod.2 + v.to_vec.to_prod.2)
+def neg_vectr {f : fm K} {s : spc K f } (v : vectr K s) : vectr K s := mk_vectr K s (-1 * v.to_vec.to_prod.2)
+def add_vectr_vectr {f : fm K} {s : spc K f } (v1 v2 : vectr K s) : vectr K s := mk_vectr K s (v1.to_vec.to_prod.2 + v2.to_vec.to_prod.2)
+def add_point_vectr {f : fm K} {s : spc K f } (p : point K s) (v : vectr K s) : point K s := mk_point K s (p.to_pt.to_prod.2 + v.to_vec.to_prod.2)
 def add_vectr_point {f : fm K} {s : spc K f } (v : vectr K s) (p : point K s) : point K s := mk_point K s (v.to_vec.to_prod.2 + p.to_pt.to_prod.2)
 def sub_point_point {f : fm K} {s : spc K f } (p1 p2 : point K s) : vectr K s := mk_vectr K s (p2.to_pt.to_prod.2 - p1.to_pt.to_prod.2)
+
+/-
+Constants
+-/
+def vectr_zero {f : fm K} (s : spc K f ) := mk_vectr K s 0
 
 /-
 
@@ -93,48 +94,24 @@ aff_vadd_vsub K
 
 instance my_affine_space : affine_space (vec K) (pt K) :=  
 aff_torsor K 
-
-end old_structure_cmd
 -/
 
 variables {f : fm K} { s : spc K f}
 
-variable v : vectr K s
-#check v        -- v : vectr K s
-#check v.1      -- v.to_vec : vec K
-#check v.1.1    -- v.to_vec.isPt : K
-#check v.1.2    -- v.to_vec.inv : v.to_vec.isPt = 0
-#check vec
-
 /-
-The following notation is uninterpretable:
-λv1 v2, ⟨⟨v1.1.1,v1.1.2,v1.coord + v2.coord⟩⟩
-Please leave explanation here of what this 
-term is/means. Use identifer rather than
-numeric projections with good names.
-
-FIX THIS CODE. IT'S TERRIBLE. See unframed file for template.
--/
+Replaced the following code. We really don't want code that looks like this.
 instance : has_add (vectr K s) := ⟨λv1 v2, ⟨⟨v1.1.1,v1.1.2,v1.coord + v2.coord⟩⟩⟩
 instance : has_zero (vectr K s) := ⟨⟨vec_zero K⟩⟩
 instance : has_neg (vectr K s) := ⟨λv, ⟨⟨v.1.1,v.1.2,-v.1.3⟩⟩⟩
-/-
-Keep this copy.
-instance : has_add (vectr K s) := ⟨λv1 v2, ⟨⟨v1.1.1,v1.1.2,v1.coord + v2.coord⟩⟩⟩
-instance : has_zero (vectr K s) := ⟨⟨vec_zero K⟩⟩
-instance : has_neg (vectr K s) := ⟨λv, ⟨⟨v.1.1,v.1.2,-v.1.3⟩⟩⟩-/
-
-/-! ### Type class instance for abelian group -/
+-/
+instance : has_add (vectr K s) := ⟨add_vectr_vectr K⟩
+instance : has_zero (vectr K s) := ⟨vectr_zero K s⟩
+instance : has_neg (vectr K s) := ⟨neg_vectr K⟩
 instance aff_comm_group_framed : add_comm_group (vectr K s) :=
 sorry
-
-
-/-! ### Scalar action -/
-
-
 @[ext]
 def vec_scalar_framed : K → vectr K s → vectr K s :=
-    λ a x, ⟨⟨x.1.1,x.1.2,a*x.1.3⟩⟩
+    mul_vectr K 
 
 instance : has_scalar K (vectr K s) := ⟨vec_scalar_framed K⟩
 
@@ -150,11 +127,11 @@ instance aff_module_framed : module K (vectr K s) := aff_semimod_framed K
 
 
 def aff_group_action_framed : vectr K s → point K s → point K s :=
-    λ x y, ⟨⟨y.1.1,y.1.2,x.1.3+y.1.3⟩⟩
+    add_vectr_point K
 
 
 def aff_group_sub_framed : point K s → point K s → vectr K s :=
-    λ x y, ⟨⟨x.1.1-y.1.1,sorry,x.1.3-y.1.3⟩⟩
+    sub_point_point K
 
 #check add_action
 
