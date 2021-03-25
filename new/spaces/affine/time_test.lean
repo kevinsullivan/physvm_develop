614d06654f6160941a4cfa0fb1fb6c804e932b7e
Kevin Sullivan <sullivan.kevinj@gail.com>
2021-03-25 19:55:26 +0000

    added discussion notes for meeting

--- new/spaces/affine/time_test.lean
+++ new/spaces/affine/time_test.lean
@@ -2,26 +2,69 @@ import .affine1K_standard
 import data.real.basic
 import .time
 
+/-
+Outstanding problems: 
+  - render C++ physics to clean physically type-checked expressions in Lean
+  - need MVP for "time" as an affine space with arbitrary affine coordinate space overlays
+    - need general library supporting (1-D) affine space API, used to implement physical time  
+    - need version of Peirce working end-to-end with terms having physical type, time
+Progress this week:
+  - assessed situation with respect to affine space and time library
+  - decided to use what I've learned from working with students to "get it done"
+  - goal: clearly implement an "affine algebra of time" as a semantic domain for Peirce
+  - approach: 
+      - MVP: pick a 1-D problem
+      - *Build from the mathematics up* rather than from some front-end preconception down
+      - Represent 1-D affine space as 2-D linear space in the usual way
+      - Instantiate vector_space typeclass for 2-D linear space (operations and notations)
+      - Build 1-D bare (no frames) affine space algebra, instsantiating affine_space typeclass
+      - Each layer of the design is *cleanly* implemented in terms of immediately preceding layer
+      - Build up from here by *extending* low-level abstractions, instantiating affine_space each time
+        - vec (linear 2-D K space)
+        - pt, vec, fm (affine 1-D K space)
+        - point, vectr, frame, coordinate space
+        - time, duration, frame, coordinate space
+      - provide examples/tests to show the algebra at each level
+    - contribution: enable progress in this project
+    - next steps: discuss after presentation
+-/
+
+abbreviation K := ℝ 
+
 -- assume unit vector is one second in standard time space
-noncomputable def t1 : time (std_space ℝ) := mk_time (std_space ℝ) (1:ℝ)    -- t=0 + 1 second
-noncomputable def t2 := mk_time (std_space ℝ) (3:ℝ)                         -- t=0 + 3 seconds
-noncomputable def d1 : duration (std_space ℝ) := t2 -ᵥ t1                     -- 2 seconds
+noncomputable def t1 : time (std_space K) := mk_time (std_space K) (1:K)    -- t=0 + 1 second
+noncomputable def t2 := mk_time (std_space K) (3:K)                         -- t=0 + 3 seconds
+noncomputable def d1 : duration (std_space K) := t2 -ᵥ t1                     -- 2 seconds
 noncomputable def t3 := 5 • d1 +ᵥ t1                                          -- t=0 + 10 seconds
 
 -- new frame with origin at t=0 + 1hr and unit duration of 1 minute
-noncomputable def t1' : time (std_space ℝ) := mk_time (std_space ℝ) (3600:ℝ)  -- t=0 + 1hr
+noncomputable def t1' : time (std_space K) := mk_time (std_space K) (3600:K)  -- t=0 + 1hr
 noncomputable def d1' := 60 • d1                                              -- one minute
 noncomputable def new_fm := mk_frame t1'.to_point d1'.to_vectr
-noncomputable def new_space := mk_space ℝ new_fm
+noncomputable def new_space := mk_space K new_fm
 
 -- create some points and vectors in new space
-noncomputable def t1'' : time new_space := mk_time new_space (1:ℝ)            -- t=0 + 1 second
-noncomputable def t2'' := mk_time new_space (3:ℝ)                             -- t=0 + 3 seconds
+noncomputable def t1'' : time new_space := mk_time new_space (1:K)            -- t=0 + 1 second
+noncomputable def t2'' := mk_time new_space (3:K)                             -- t=0 + 3 seconds
 noncomputable def d1'' := t2'' -ᵥ t1''                                        -- 2 seconds
 noncomputable def t3'' := 5 • d1'' +ᵥ t1''
 
 -- check for type errors across difference coordinate systems on time
-noncomputable def d_good := t1' -ᵥ t1
-noncomputable def t_good := d1' +ᵥ t1
-noncomputable def d_bad := t1'' -ᵥ t1
-noncomputable def t_bad := d1'' +ᵥ t1
+noncomputable def d_good := t1' -ᵥ t1   -- time - time = duration
+noncomputable def t_good := d1' +ᵥ t1   -- duration + time = time
+noncomputable def d_bad := t1'' -ᵥ t1   -- but not if arguments are in different frames/spaces
+noncomputable def t_bad := d1'' +ᵥ t1   -- but not if arguments are in different frames/spaces
+
+/-
+Thoughts
+-/
+
+-- time.lean really belongs to phys layer
+-- get transforms working in this design
+-- support end-to-end in Peirce
+
+
+-- predefine a std_time space?
+-- add helper functions to access coordinates directly
+-- get affine combinations working (not high priority)
+-- think about how this work might enable re-imagining tf-like API
