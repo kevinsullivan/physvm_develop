7a62ca7e261826eb14446fa831244829e4cd6192
Kevin Sullivan <sullivan.kevinj@gail.com>
2021-03-27 19:42:49 +0000

    start to transition geom

--- new/phys/geom/geom.lean
+++ new/phys/geom/geom.lean
@@ -6,6 +6,7 @@ open_locale affine
 /-
 Framed points, vectors, frames
 -/
+namespace geom
 
 universes u --v w
 variables 
@@ -15,50 +16,48 @@ variables
 Add frames and (coordinate) spaces based on frames
 -/
 
-structure time {f : fm K} (s : spc K f ) extends point s 
-def mk_time' {f : fm K} (s : spc K f ) (p : point s) : time s := time.mk p  
-def mk_time {f : fm K} (s : spc K f ) (k : K) : time s := time.mk (mk_point s k) 
+structure position {f : fm K} (s : spc K f ) extends point s 
+def mk_position' {f : fm K} (s : spc K f ) (p : point s) : position s := position.mk p  
+def mk_position {f : fm K} (s : spc K f ) (k : K) : position s := position.mk (mk_point s k) 
 
-structure duration {f : fm K} (s : spc K f ) extends vectr s 
-def mk_duration' {f : fm K} (s : spc K f ) (v : vectr s) : duration s := duration.mk v
-def mk_duration  {f : fm K} (s : spc K f ) (k : K) : duration s := duration.mk (mk_vectr s k) 
+structure translation {f : fm K} (s : spc K f ) extends vectr s 
+def mk_duration' {f : fm K} (s : spc K f ) (v : vectr s) : translation s := translation.mk v
+def mk_duration  {f : fm K} (s : spc K f ) (k : K) : translation s := translation.mk (mk_vectr s k) 
 
 -- note that we don't extend fm
-def mk_time_frame {parent : fm K} {s : spc K parent} (p : time s) (v : duration s) :=
+def mk_time_frame {parent : fm K} {s : spc K parent} (p : position s) (v : translation s) :=
 fm.deriv (p.to_point.to_pt, v.to_vectr.to_vec) parent   -- TODO: make sure v ≠ 0
 
 
 /-
     *************************************
-    Instantiate vector_space K (vector K)
+    Instantiate vector_space K (translation K)
     *************************************
 -/
 
-namespace geom
-
 variables {f : fm K} {s : spc K f } 
 
-def add_vectr_vectr (v1 v2 : duration s) : duration s := 
+def add_vectr_vectr (v1 v2 : translation s) : translation s := 
     mk_duration' s (v1.to_vectr + v2.to_vectr)
-def smul_vectr (k : K) (v : duration s) : duration s := 
+def smul_vectr (k : K) (v : translation s) : translation s := 
     mk_duration' s (k • v.to_vectr)
-def neg_vectr (v : duration s) : duration s := 
+def neg_vectr (v : translation s) : translation s := 
     mk_duration' s ((-1 : K) • v.to_vectr)
-def sub_vectr_vectr (v1 v2 : duration s) : duration s :=    -- v1-v2
+def sub_vectr_vectr (v1 v2 : translation s) : translation s :=    -- v1-v2
     add_vectr_vectr v1 (neg_vectr v2)
 
 -- See unframed file for template for proving vector_space
 
-instance has_add_vectr : has_add (duration s) := ⟨ add_vectr_vectr ⟩
-lemma add_assoc_vectr : ∀ a b c : duration s, a + b + c = a + (b + c) := sorry
-instance add_semigroup_vectr : add_semigroup (duration s) := ⟨ add_vectr_vectr, add_assoc_vectr ⟩ 
+instance has_add_vectr : has_add (translation s) := ⟨ add_vectr_vectr ⟩
+lemma add_assoc_vectr : ∀ a b c : translation s, a + b + c = a + (b + c) := sorry
+instance add_semigroup_vectr : add_semigroup (translation s) := ⟨ add_vectr_vectr, add_assoc_vectr ⟩ 
 
 def vectr_zero  := mk_duration s 0
-instance has_zero_vectr : has_zero (duration s) := ⟨vectr_zero⟩
+instance has_zero_vectr : has_zero (translation s) := ⟨vectr_zero⟩
 
-lemma zero_add_vectr : ∀ a : duration s, 0 + a = a := sorry
-lemma add_zero_vectr : ∀ a : duration s, a + 0 = a := sorry
-instance add_monoid_vectr : add_monoid (duration s) := ⟨ 
+lemma zero_add_vectr : ∀ a : translation s, 0 + a = a := sorry
+lemma add_zero_vectr : ∀ a : translation s, a + 0 = a := sorry
+instance add_monoid_vectr : add_monoid (translation s) := ⟨ 
     -- add_semigroup
     add_vectr_vectr, 
     add_assoc_vectr, 
@@ -69,18 +68,18 @@ instance add_monoid_vectr : add_monoid (duration s) := ⟨
     add_zero_vectr
 ⟩
 
-instance has_neg_vectr : has_neg (duration s) := ⟨neg_vectr⟩
-instance has_sub_vectr : has_sub (duration s) := ⟨ sub_vectr_vectr ⟩ 
-lemma sub_eq_add_neg_vectr : ∀ a b : duration s, a - b = a + -b := sorry
-instance sub_neg_monoid_vectr : sub_neg_monoid (duration s) := ⟨ 
+instance has_neg_vectr : has_neg (translation s) := ⟨neg_vectr⟩
+instance has_sub_vectr : has_sub (translation s) := ⟨ sub_vectr_vectr ⟩ 
+lemma sub_eq_add_neg_vectr : ∀ a b : translation s, a - b = a + -b := sorry
+instance sub_neg_monoid_vectr : sub_neg_monoid (translation s) := ⟨ 
     add_vectr_vectr, add_assoc_vectr, vectr_zero, zero_add_vectr, add_zero_vectr, -- add_monoid
     neg_vectr,                                                                  -- has_neg
     sub_vectr_vectr,                                                              -- has_sub
     sub_eq_add_neg_vectr,                                                       -- new
 ⟩ 
 
-lemma add_left_neg_vectr : ∀ a : duration s, -a + a = 0 := sorry
-instance : add_group (duration s) := ⟨
+lemma add_left_neg_vectr : ∀ a : translation s, -a + a = 0 := sorry
+instance : add_group (translation s) := ⟨
     -- sub_neg_monoid
     add_vectr_vectr, add_assoc_vectr, vectr_zero, zero_add_vectr, add_zero_vectr, -- add_monoid
     neg_vectr,                                                                  -- has_neg
@@ -90,15 +89,15 @@ instance : add_group (duration s) := ⟨
     add_left_neg_vectr,
 ⟩ 
 
-lemma add_comm_vectr : ∀ a b : duration s, a + b = b + a := sorry
-instance add_comm_semigroup_vectr : add_comm_semigroup (duration s) := ⟨
+lemma add_comm_vectr : ∀ a b : translation s, a + b = b + a := sorry
+instance add_comm_semigroup_vectr : add_comm_semigroup (translation s) := ⟨
     -- add_semigroup
     add_vectr_vectr, 
     add_assoc_vectr,
     add_comm_vectr,
 ⟩
 
-instance add_comm_monoid_vectr : add_comm_monoid (duration s) := ⟨
+instance add_comm_monoid_vectr : add_comm_monoid (translation s) := ⟨
 -- add_monoid
     -- add_semigroup
     add_vectr_vectr, 
@@ -112,30 +111,30 @@ instance add_comm_monoid_vectr : add_comm_monoid (duration s) := ⟨
     add_comm_vectr,
 ⟩
 
-instance has_scalar_vectr : has_scalar K (duration s) := ⟨
+instance has_scalar_vectr : has_scalar K (translation s) := ⟨
 smul_vectr,
 ⟩
 
-lemma one_smul_vectr : ∀ b : duration s, (1 : K) • b = b := sorry
-lemma mul_smul_vectr : ∀ (x y : K) (b : duration s), (x * y) • b = x • y • b := sorry
-instance mul_action_vectr : mul_action K (duration s) := ⟨
+lemma one_smul_vectr : ∀ b : translation s, (1 : K) • b = b := sorry
+lemma mul_smul_vectr : ∀ (x y : K) (b : translation s), (x * y) • b = x • y • b := sorry
+instance mul_action_vectr : mul_action K (translation s) := ⟨
 one_smul_vectr,
 mul_smul_vectr,
 ⟩ 
 
-lemma smul_add_vectr : ∀(r : K) (x y : duration s), r • (x + y) = r • x + r • y := sorry
-lemma smul_zero_vectr : ∀(r : K), r • (0 : duration s) = 0 := sorry
-instance distrib_mul_action_K_vectrKx : distrib_mul_action K (duration s) := ⟨
+lemma smul_add_vectr : ∀(r : K) (x y : translation s), r • (x + y) = r • x + r • y := sorry
+lemma smul_zero_vectr : ∀(r : K), r • (0 : translation s) = 0 := sorry
+instance distrib_mul_action_K_vectrKx : distrib_mul_action K (translation s) := ⟨
 smul_add_vectr,
 smul_zero_vectr,
 ⟩ 
 
 -- renaming vs template due to clash with name "s" for prevailing variable
-lemma add_smul_vectr : ∀ (a b : K) (x : duration s), (a + b) • x = a • x + b • x := sorry
-lemma zero_smul_vectr : ∀ (x : duration s), (0 : K) • x = 0 := sorry
-instance semimodule_K_durationK : semimodule K (duration s) := ⟨ add_smul_vectr, zero_smul_vectr ⟩ 
+lemma add_smul_vectr : ∀ (a b : K) (x : translation s), (a + b) • x = a • x + b • x := sorry
+lemma zero_smul_vectr : ∀ (x : translation s), (0 : K) • x = 0 := sorry
+instance semimodule_K_durationK : semimodule K (translation s) := ⟨ add_smul_vectr, zero_smul_vectr ⟩ 
 
-instance add_comm_group_vectr : add_comm_group (duration s) := ⟨
+instance add_comm_group_vectr : add_comm_group (translation s) := ⟨
 -- add_group
     add_vectr_vectr, add_assoc_vectr, vectr_zero, zero_add_vectr, add_zero_vectr, -- add_monoid
     neg_vectr,                                                                  -- has_neg
@@ -146,7 +145,7 @@ instance add_comm_group_vectr : add_comm_group (duration s) := ⟨
     add_comm_vectr,
 ⟩
 
-instance : vector_space K (duration s) := @geom.semimodule_K_durationK K _ _ f s
+instance : vector_space K (translation s) := @geom.semimodule_K_durationK K _ _ f s
 
 
 /-
@@ -159,37 +158,37 @@ instance : vector_space K (duration s) := @geom.semimodule_K_durationK K _ _ f s
 /-
 Affine operations
 -/
-instance : has_add (duration s) := ⟨add_vectr_vectr⟩
-instance : has_zero (duration s) := ⟨vectr_zero⟩
-instance : has_neg (duration s) := ⟨neg_vectr⟩
+instance : has_add (translation s) := ⟨add_vectr_vectr⟩
+instance : has_zero (translation s) := ⟨vectr_zero⟩
+instance : has_neg (translation s) := ⟨neg_vectr⟩
 
 /-
 Lemmas needed to implement affine space API
 -/
 
-def sub_point_point {f : fm K} {s : spc K f } (p1 p2 : time s) : duration s := 
+def sub_point_point {f : fm K} {s : spc K f } (p1 p2 : position s) : translation s := 
     mk_duration' s (p2.to_point -ᵥ p1.to_point)
-def add_point_vectr {f : fm K} {s : spc K f } (p : time s) (v : duration s) : time s := 
-    mk_time' s (v.to_vectr +ᵥ p.to_point) -- reorder assumes order is irrelevant
-def add_vectr_point {f : fm K} {s : spc K f } (v : duration s) (p : time s) : time s := 
-    mk_time' s (v.to_vectr +ᵥ p.to_point)
+def add_point_vectr {f : fm K} {s : spc K f } (p : position s) (v : translation s) : position s := 
+    mk_position' s (v.to_vectr +ᵥ p.to_point) -- reorder assumes order is irrelevant
+def add_vectr_point {f : fm K} {s : spc K f } (v : translation s) (p : position s) : position s := 
+    mk_position' s (v.to_vectr +ᵥ p.to_point)
 
-def aff_vectr_group_action : duration s → time s → time s := add_vectr_point
-instance : has_vadd (duration s) (time s) := ⟨aff_vectr_group_action⟩
+def aff_vectr_group_action : translation s → position s → position s := add_vectr_point
+instance : has_vadd (translation s) (position s) := ⟨aff_vectr_group_action⟩
 
-lemma zero_vectr_vadd'_a1 : ∀ p : time s, (0 : duration s) +ᵥ p = p := sorry
-lemma vectr_add_assoc'_a1 : ∀ (g1 g2 : duration s) (p : time s), g1 +ᵥ (g2 +ᵥ p) = (g1 + g2) +ᵥ p := sorry
-instance vectr_add_action: add_action (duration s) (time s) := 
+lemma zero_vectr_vadd'_a1 : ∀ p : position s, (0 : translation s) +ᵥ p = p := sorry
+lemma vectr_add_assoc'_a1 : ∀ (g1 g2 : translation s) (p : position s), g1 +ᵥ (g2 +ᵥ p) = (g1 + g2) +ᵥ p := sorry
+instance vectr_add_action: add_action (translation s) (position s) := 
 ⟨ aff_vectr_group_action, zero_vectr_vadd'_a1, vectr_add_assoc'_a1 ⟩ 
 
-def aff_point_group_sub : time s → time s → duration s := sub_point_point
-instance point_has_vsub : has_vsub (duration s) (time s) := ⟨ aff_point_group_sub ⟩ 
+def aff_point_group_sub : position s → position s → translation s := sub_point_point
+instance point_has_vsub : has_vsub (translation s) (position s) := ⟨ aff_point_group_sub ⟩ 
 
-instance : nonempty (time s) := ⟨mk_time s 0⟩
+instance : nonempty (position s) := ⟨mk_position s 0⟩
 
-lemma point_vsub_vadd_a1 : ∀ (p1 p2 : (time s)), (p1 -ᵥ p2) +ᵥ p2 = p1 := sorry
-lemma point_vadd_vsub_a1 : ∀ (g : duration s) (p : time s), g +ᵥ p -ᵥ p = g := sorry
-instance aff_point_torsor : add_torsor (duration s) (time s) := 
+lemma point_vsub_vadd_a1 : ∀ (p1 p2 : (position s)), (p1 -ᵥ p2) +ᵥ p2 = p1 := sorry
+lemma point_vadd_vsub_a1 : ∀ (g : translation s) (p : position s), g +ᵥ p -ᵥ p = g := sorry
+instance aff_point_torsor : add_torsor (translation s) (position s) := 
 ⟨ 
     aff_vectr_group_action,
     zero_vectr_vadd'_a1,    -- add_action
@@ -201,6 +200,6 @@ instance aff_point_torsor : add_torsor (duration s) (time s) :=
 
 open_locale affine
 
-instance : affine_space (duration s) (time s) := @geom.aff_point_torsor K _ _ f s
+instance : affine_space (translation s) (position s) := @geom.aff_point_torsor K _ _ f s
 
 end geom
--- new/phys/geom/geom_test.lean
+++ new/phys/geom/geom_test.lean
@@ -1,50 +1,29 @@
 import data.real.basic
-import .time
+import .geom
 
 /-
-Outstanding problems: 
-  - render C++ physics to clean physically type-checked expressions in Lean
-  - need MVP for "time" as an affine space with arbitrary affine coordinate space overlays
-    - need general library supporting (1-D) affine space API, used to implement physical time  
-    - need version of Peirce working end-to-end with terms having physical type, time
-Progress this week:
-  - assessed situation with respect to affine space and time library
-  - decided to use what I've learned from working with students to "get it done"
-  - goal: clearly implement an "affine algebra of time" as a semantic domain for Peirce
-  - approach: 
-      - MVP: pick a 1-D problem
-      - *Build from the mathematics up* rather than from some front-end preconception down
-      - Represent 1-D affine space as 2-D linear space in the usual way
-      - Instantiate vector_space typeclass for 2-D linear space (operations and notations)
-      - Build 1-D bare (no frames) affine space algebra, instantiating affine_space typeclass
-      - Each layer of the design is *cleanly* implemented in terms of immediately preceding layer
-      - Build up from here by *extending* low-level abstractions, instantiating affine_space each time
-        - vc (linear 2-D K space)
-        - pt, vec, fm (affine 1-D K space)
-        - point, vectr, frame, coordinate space
-        - time, duration, frame, coordinate space
-      - provide examples/tests to show the algebra at each level
-    - contribution: enable progress in this project
-    - next steps: discuss after presentation
+Comments in rest of file are out of date, written for time not geom. But same reasoning applies.
 -/
+
+open geom
 
 abbreviation K := ℝ   -- or try ℚ for a computable version (but no square roots)
 
 -- assume unit vector is one second in standard time space
-noncomputable def t1 : time (std_space K) := mk_time (std_space K) 1          -- t=0 + 1 second
-noncomputable def t2 := mk_time (std_space K) 3                               -- t=0 + 3 seconds
-noncomputable def d1 : duration (std_space K) := t2 -ᵥ t1                     -- 2 seconds
+noncomputable def t1 : position (std_space K) := mk_position (std_space K) 1          -- t=0 + 1 second
+noncomputable def t2 := mk_position (std_space K) 3                               -- t=0 + 3 seconds
+noncomputable def d1 : translation (std_space K) := t2 -ᵥ t1                     -- 2 seconds
 noncomputable def t3 := 5 • d1 +ᵥ t1                                          -- t=0 + 11 seconds
 
 -- new frame with origin at t=0 + 1hr and unit duration of 1 minute
-noncomputable def t1' : time (std_space K) := mk_time (std_space K) 3600      -- t=0 + 1hr
+noncomputable def t1' : position (std_space K) := mk_position (std_space K) 3600      -- t=0 + 1hr
 noncomputable def d1' := 60 • (mk_duration (std_space K) 1)                                             -- one minute
 noncomputable def new_fm := mk_frame t1'.to_point d1'.to_vectr                -- TODO: fix
 noncomputable def new_space := mk_space K new_fm
 
 -- create some points and vectors in new space
-noncomputable def t1'' : time new_space := mk_time new_space 1                -- t=0 + 1 second (3660)
-noncomputable def t2'' := mk_time new_space 3                                 -- t=0 + 3 seconds (3600 + 180)
+noncomputable def t1'' : position new_space := mk_position new_space 1                -- t=0 + 1 second (3660)
+noncomputable def t2'' := mk_position new_space 3                                 -- t=0 + 3 seconds (3600 + 180)
 noncomputable def d1'' := t2'' -ᵥ t1''                                        -- 2 seconds
 noncomputable def t3'' := 5 • d1'' +ᵥ t1''                                    -- 11 in the new space, what old?
 
@@ -58,10 +37,11 @@ noncomputable def t_bad := 8 • d1'' +ᵥ t1 -- error: no impl of +ᵥ across a
 Afterthoughts
 -/
 
--- fill in the proofs (low risk, low priority; postpone)
--- get transforms working in this design (design proposal needed)
--- support end-to-end in Peirce (schedule discussion for lang-level design)
+-- #1: support end-to-end in Peirce (schedule discussion for lang-level design)
+-- #2: get transforms working in this design (design proposal needed)
+-- #3: re-imagine a tf-like API based on work in this style
 
--- add helper functions to access coordinates directly
+-- fill in the proofs (low risk, low priority; postpone)
+-- add helper functions to access coordinate values
 -- get affine combinations working (not high priority)
--- re-imagine a tf-like API based on work in this style
+-- fix that one problem where I break an abstraction boundry for lack of an at-level mk function
