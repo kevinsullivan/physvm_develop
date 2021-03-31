d070e2f60224bb760307b298197d72c7aefca0ab
Kevin Sullivan <sullivan.kevinj@gail.com>
2021-03-31 00:53:18 +0000

    delete commented previous version

--- new/lang/expressions/time_expr.lean
+++ new/lang/expressions/time_expr.lean
@@ -6,8 +6,12 @@ import ...phys.time.time
 namespace lang.time
 universes u
 
-variables (K : Type u) [field K] [inhabited K] {f : fm K TIME} {sp : spc K f} {f1 : fm K TIME} {f2 : fm K TIME}
+variables 
+  (K : Type u) [field K] [inhabited K] 
+  {f : fm K TIME} {sp : spc K f} 
+  --{f1 : fm K TIME} {f2 : fm K TIME}
 -- {K : Type u} [field K] [inhabited K] {f : fm K TIME}
+
 
 /-
 Duration
@@ -76,51 +80,5 @@ def env.init : env sp :=
     (λv, ⟨mk_vectr sp 1⟩),
     (λv, ⟨mk_point sp 0⟩)
   ⟩
+
 end lang.time
-
-/---import .expr_base
---import data.real.basic
-import .expr_base
---import ...math.aff1Kcoord.aff1Kcoord_std
-import ...phys.time.time
-
-
-namespace lang.time
-universes u
-
-variables (K : Type u) [field K] [inhabited K] {f : fm K TIME} {sp : spc K f} 
-
-structure duration_var extends var 
-
-structure time_var extends var
-
-inductive duration_expr (sp : spc K f) : Type u
-| lit (v : duration sp) : duration_expr
-| var (v : duration_var) : duration_expr
-
-abbreviation duration_env  := 
-  duration_var → duration sp
-
-abbreviation duration_eval := 
-  @duration_env K _ _ f sp → duration_var → duration sp
-
-inductive time_expr (sp : spc K f) : Type u
-| lit (p : time sp) : time_expr
-| var (v : time_var) : time_expr
-
-abbreviation time_env  := 
-  time_var → time sp
-
-abbreviation time_eval := 
-  @time_env K _ _ f sp → time_var → time sp
-
-structure env :=
-  (d : @duration_env K _ _ f sp )
-  (t : @time_env K _ _ f sp )
-
-def env.init : @env K _ _ f sp :=
-  ⟨
-    (λv, ⟨mk_vectr sp 1⟩),
-    (λv, ⟨mk_point sp 0⟩)
-  ⟩
-end lang.time-/
